3e8babd294589d07154baf57c29e54a98c5607da
Your Name <ayomideflex72@gmail.com>
2025-06-22 19:09:45 +0100

    update-lib

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashBoardLayout({ children }: { children: ReactNode }) {
       <SideBar>
         <SignOut />
       </SideBar>
-      <div className="mx-auto min-h-screen max-w-6xl pt-8">
+      <div className="mx-auto min-h-screen px-4 md:px-6 pt-8">
         {children}
       </div>
     </div>
